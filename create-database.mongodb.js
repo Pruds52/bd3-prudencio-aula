c27e6e2aa47b026fe53579e28fb0a4cc27c8fb15
Pruds52 <prudenciopvp@gmail.com>
2024-08-06 20:35:59 -0300

    Criação da base de dados.

---
+++ create-database.mongodb.js
@@ -0,0 +1,12 @@
+// NOME DO BANCO DE DADOS
+const database = 'BD3-PRUDENCIO-AULA';
+
+// NOME DO BANCO DA COLEÇÃO DE DADOS
+const collection = 'LIVRARIA';
+
+// CRIAR OU ACESSAR O BANCO DE DADOS:
+use(database);
+
+// CRIAR UMA COLEÇAÕ DE DADOS;
+db.createCollection(collection);
+
